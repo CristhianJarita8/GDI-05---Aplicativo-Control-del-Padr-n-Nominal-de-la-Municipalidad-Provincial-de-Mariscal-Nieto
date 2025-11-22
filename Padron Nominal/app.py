498227ee4fa5a48e27cf7f5b2f72e9397df35f31
Cristhian Jarita <142757911+CristhianJarita8@users.noreply.github.com>
2025-11-22 08:12:26 -0500

    Delete Padron Nominal directory

--- Padron Nominal/app.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-from ui.theme import init_theme
-from ui.main_window import MainMenu
-
-if __name__ == "__main__":
-    root = MainMenu()
-    init_theme(root)
-    root.mainloop()
